e14e43eb22eb577a5088fdf5919a4a33b615372c
Yaroslav Dyhanov <duhanov2003@gmail.com>
2021-09-13 08:52:41 +0300

    adding changes

--- Lab_1/lab1_3.py
+++ Lab_1/lab1_3.py
@@ -1,30 +1,29 @@
 import sys
-import re
+from ultrasplit import split_by_symbol
 
 
-def get_express_result(experssion, index=0):
+def get_express_result(expression, index=0):
     """ return if correct nat operations and its result. """
 
     signs = "+-*/"
-    if expression and experssion[0] not in signs:
-        experssion = "+" + experssion
-    else:
-        return (False, None)
+    if expression[0] not in signs:
+        expression = "+" + expression
 
-    if not isinstance(experssion, list):
-        experssion = re.findall(re.compile('(\d+|[^ 0-9])'), experssion) #split by each symbol
+    if not isinstance(expression, list):
+        expression = split_by_symbol(expression)  # split by each symbol
     try:
-        if experssion[index] in signs:
-            if (experssion[index+1]).isdigit():
-                return get_express_result(experssion, index + 2) #will be analize next two symbols
+        if expression[index] in signs:
+            if (expression[index+1]).isdigit():
+                # will be analize next two symbols
+                return get_express_result(expression, index + 2)
             raise ValueError
         else:
             raise ValueError
     except ValueError:
         return (False, None)
-    except IndexError: #there was no any problem
-        return (True, eval("".join(i for i in experssion)))
+    except IndexError:  # there was no any problem
+        return (True, eval("".join(i for i in expression)))
 
 
 expression = "".join(i for i in sys.argv[1:])
-print(get_express_result(expression))
+print(get_express_result(expression) if expression else (False, None))
